fbba4f403862a24a5c0f95db6dab3b5990a19397
JG20108 <joseosgui@hotmail.com>
2024-02-20 10:28:13 -0600

    Improvements on the RulesModal

--- src/RulesModal.jsx
+++ src/RulesModal.jsx
@@ -11,9 +11,9 @@ function RulesModal({ isOpen, onClose }) {
           <h2>Galactic Battle: Rock Paper Scissors!</h2>
           <p>Prepare to clash in the ultimate game of wit and strategy, where cosmic forces collide. Here&apos;s how to become a Rock Paper Scissors champion:</p>
           <ul>
-            <li><strong>Rock crushes Scissors:</strong> Unleash the mountain&apos;s might to shatter blades!</li>
-            <li><strong>Paper covers Rock:</strong> Envelop the stone with the wisdom of ages!</li>
-            <li><strong>Scissors cuts Paper:</strong> Slice through knowledge with the edge of destiny!</li>
+            <li><span className="rock-text"><strong>Rock</strong></span> crushes <span className="scissors-text"><strong>Scissors</strong></span>: Unleash the mountain&apos;s might to shatter blades!</li>
+            <li><span className="paper-text"><strong>Paper</strong></span> covers <span className="rock-text"><strong>Rock</strong></span>: Envelop the stone with the wisdom of ages!</li>
+            <li><span className="scissors-text"><strong>Scissors</strong></span> cuts <span className="paper-text"><strong>Paper</strong></span>: Slice through knowledge with the edge of destiny!</li>
           </ul>
           <p>Choose your weapon wisely, as your fate is sealed with your decision. The first to conquer their opponent in three cosmic duels shall be crowned the ruler of the galaxy...</p>
           <h2> May the stars align in your favor!</h2>
